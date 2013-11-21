1dd0d597f99d7c6ad3b4dfcdd5ef158b71e1aea9
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2013-11-21 16:15:01 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lmstats::Application.config.secret_key_base = '525fe15c51cad3475f3b1d7bdae4129000007e4a52c3632d5758fe9af6b76345a2acb10e02153fa760dc73d292a004f66058252e7b312a4b25096563147327f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lmstats::Application.config.session_store :cookie_store, key: '_lmstats_session'
